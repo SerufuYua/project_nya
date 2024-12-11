0ed5623d2662c5e0f1d18662e1d5bd3dd15c6042
@serufu_yua <serufu_yua@nya.com>
2024-12-11 09:07:07 -0800

    add procedure UpdateCamera

--- code/mythirdpersonnavigation.pas
+++ code/mythirdpersonnavigation.pas
@@ -11,6 +11,8 @@ uses
 type
   TMyThirdPersonNavigation = class(TCastleMouseLookNavigation)
   protected
+    FDistanceToAvatarTarget: Single;
+    FCameraSpeed: Single;
     FAvatarTarget: TVector3;
     FAvatarTargetPersistent: TCastleVector3Persistent;
     FAvatarHierarchy: TCastleTransform;
@@ -19,9 +21,14 @@ type
     procedure SetAvatarHierarchy(const Value: TCastleTransform);
     function GetAvatarTargetForPersistent: TVector3;
     procedure SetAvatarTargetForPersistent(const AValue: TVector3);
+    procedure SetDistanceToAvatarTarget(const Value: Single);
+
+    procedure UpdateCamera(const SecondsPassed: Single);
   public
     const
       DefaultAvatarTarget: TVector3 = (X: 0; Y: 20; Z: 0);
+      DefaultDistanceToAvatarTarget = 40.0;
+      DefaultCameraSpeed = 10;
 
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
@@ -33,6 +40,10 @@ type
   published
     property AvatarHierarchy: TCastleTransform read FAvatarHierarchy write SetAvatarHierarchy;
     property AvatarTargetPersistent: TCastleVector3Persistent read FAvatarTargetPersistent;
+    property DistanceToAvatarTarget: Single read FDistanceToAvatarTarget write SetDistanceToAvatarTarget
+      {$ifdef FPC}default DefaultDistanceToAvatarTarget{$endif};
+    property CameraSpeed: Single read FCameraSpeed write FCameraSpeed
+      {$ifdef FPC}default DefaultCameraSpeed{$endif};
   end;
 
 implementation
@@ -43,7 +54,9 @@ uses
 constructor TMyThirdPersonNavigation.Create(AOwner: TComponent);
 begin
   inherited;
-  FAvatarTarget := DefaultAvatarTarget;
+  FAvatarTarget:= DefaultAvatarTarget;
+  FDistanceToAvatarTarget:= DefaultDistanceToAvatarTarget;
+  FCameraSpeed := DefaultCameraSpeed;
 
   FAvatarHierarchyFreeObserver:= TFreeNotificationObserver.Create(Self);
   FAvatarHierarchyFreeObserver.OnFreeNotification:= {$ifdef FPC}@{$endif}AvatarHierarchyFreeNotification;
@@ -67,6 +80,9 @@ procedure TMyThirdPersonNavigation.Update(const SecondsPassed: Single;
                  var HandleInput: Boolean);
 begin
   inherited;
+  if not Valid then Exit;
+
+  UpdateCamera(SecondsPassed);
 end;
 
 procedure TMyThirdPersonNavigation.SetAvatarHierarchy(const Value: TCastleTransform);
@@ -81,11 +97,29 @@ end;
 function TMyThirdPersonNavigation.PropertySections(const PropertyName: String): TPropertySections;
 begin
   if ArrayContainsString(PropertyName, [
-       'AvatarHierarchy', 'AvatarTargetPersistent'
+       'AvatarHierarchy', 'AvatarTargetPersistent',
+       'DistanceToAvatarTarget'
      ]) then
     Result := [psBasic]
   else
     Result := inherited PropertySections(PropertyName);
+end;
+
+procedure TMyThirdPersonNavigation.UpdateCamera(const SecondsPassed: Single);
+var
+  TargetWorldPos: TVector3;
+  CameraPos, CameraDir, CameraUp: TVector3;
+  CameraPosTarget: TVector3;
+begin
+  if NOT Assigned(AvatarHierarchy) then Exit;
+
+  TargetWorldPos:= AvatarHierarchy.WorldTransform.MultPoint(AvatarTarget);
+  Camera.GetWorldView(CameraPos, CameraDir, CameraUp);
+  CameraPosTarget:= TargetWorldPos - CameraDir * DistanceToAvatarTarget;
+
+  CameraPos:= SmoothTowards(CameraPos, CameraPosTarget, SecondsPassed, CameraSpeed);
+
+  Camera.SetWorldView(CameraPos, CameraDir, CameraUp);
 end;
 
 procedure TMyThirdPersonNavigation.AvatarHierarchyFreeNotification(
@@ -102,6 +136,14 @@ end;
 procedure TMyThirdPersonNavigation.SetAvatarTargetForPersistent(const AValue: TVector3);
 begin
   AvatarTarget:= AValue;
+end;
+
+procedure TMyThirdPersonNavigation.SetDistanceToAvatarTarget(const Value: Single);
+begin
+  if FDistanceToAvatarTarget <> Value then
+  begin
+    FDistanceToAvatarTarget:= Value;
+  end;
 end;
 
 initialization
