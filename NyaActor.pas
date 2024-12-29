f6c0c9aff0bacea07d0b8041ff21d03e91810ac9
@serufu_yua <serufu_yua@nya.com>
2024-12-29 09:01:43 +0500

    added property Url

--- NyaActor.pas
+++ NyaActor.pas
@@ -10,9 +10,14 @@ uses
 
 type
   TNyaActor = class(TCastleTransform)
+  protected
+    FUrl: String;
+    procedure SetUrl(const Value: String);
   public
+    constructor Create(AOwner: TComponent); override;
     function PropertySections(const PropertyName: String): TPropertySections; override;
-
+  published
+    property Url: String read FUrl write SetUrl;
   end;
 
 implementation
@@ -20,10 +25,24 @@ implementation
 uses
   CastleComponentSerialize, CastleUtils;
 
+constructor TNyaActor.Create(AOwner: TComponent);
+begin
+  inherited;
+
+  FUrl:= '';
+end;
+
+procedure TNyaActor.SetUrl(const value: String);
+begin
+  if (FUrl = value) then Exit;
+  FUrl:= value;
+
+end;
+
 function TNyaActor.PropertySections(const PropertyName: String): TPropertySections;
 begin
   if ArrayContainsString(PropertyName, [
-       'none'
+       'Url'
      ]) then
     Result:= [psBasic]
   else
