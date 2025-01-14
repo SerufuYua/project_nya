e1799d282f9cb50a8cb4f5280c1d71070ec529c6
@serufu_yua <serufu_yua@nya.com>
2025-01-14 15:17:43 -0800

    switch character animations

--- code/gameviewtravel.pas
+++ code/gameviewtravel.pas
@@ -4,14 +4,14 @@ interface
 
 uses Classes, BaseViewTravel,
   CastleVectors, CastleUIControls, CastleControls, CastleKeysMouse,
-  CastleTransform, NyaActorChara,
-  NyaSwitch;
+  CastleTransform, NyaActorChara, NyaSwitch;
 
 type
   TViewTravel = class(TBaseViewPlay)
   public
     procedure Start; override;
   protected
+    FActorBoy: TNyaActorChara;
     procedure DoTouchSwitch(const Sender: TObject; Touch: Boolean); override;
     procedure DoActivateSwitch(Sender: TObject); override;
   end;
@@ -31,16 +31,33 @@ begin
   { set map }
   Map.Url:= 'castle-data:/MapTravel_ContainerRoom.castle-user-interface';
 
-  { se Girl Character }
+  { set Girl Character }
   FActorMain:= Map.DesignedComponent('CharaGirl') as TNyaActorChara;
+
+  { set Boy Character }
+  FActorBoy:= Map.DesignedComponent('CharaBoy') as TNyaActorChara;
 
   inherited;
 end;
 
 procedure TViewTravel.DoTouchSwitch(const Sender: TObject; Touch: Boolean);
+var
+  switch: TNyaSwitch;
 begin
   inherited;
-  { any additional actions for some specail switch }
+
+  switch:= Sender as TNyaSwitch;
+  if NOT Assigned(switch) then Exit;
+
+  Case switch.Name of
+  'BoySwitch':
+    begin
+      if Touch then
+        FActorBoy.AutoAnimation:= 'GAME.INROOM.SEAT_WITH_PDA.LOOKING'
+      else
+        FActorBoy.AutoAnimation:= 'GAME.INROOM.SEAT_WITH_PDA.WORKING';
+    end;
+  end;
 end;
 
 procedure TViewTravel.DoActivateSwitch(Sender: TObject);
@@ -56,11 +73,17 @@ begin
       Notifications.Show('Lets play with my Toy!');
       FGetToGo:= ViewPlayGirl;
     end;
-  'BedSwitch':
+  'BoySwitch':
     begin
-      Notifications.Show('Lets play in my Bed!');
+      Notifications.Show('Lets play Together!');
       FGetToGo:= ViewPlayTogether;
     end;
+  { #todo : Solo Play Scene }
+{  'BedSwitch':
+    begin
+      Notifications.Show('Lets play in my Bed!');
+      FGetToGo:= ViewPlaySolo;
+    end;}
   else
     Notifications.Show('There is nothing to use');
   end;
