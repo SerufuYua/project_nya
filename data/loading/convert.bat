edae4634e442279270f7fe60b5052db1a14ac79c
@serufu_yua <serufu_yua@nya.com>
2025-03-01 17:25:34 +0500

    added to git *.bat and *.txt files

---
+++ data/loading/convert.bat
@@ -0,0 +1,2 @@
+C:\castle_game_engine\bin\image-to-pascal.exe GameEmbeddedImages %1
+pause
---
+++ start.bat
@@ -0,0 +1,5 @@
+@echo off
+rem start prjNya.exe --Home
+rem start prjNya.exe --PlayGirl
+rem start prjNya.exe --PlayTogether
+start prjNya.exe --Asteroid
