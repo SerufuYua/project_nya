fa997196966cb764fe89f3cccf5874d9231ea60f
@serufu_yua <serufu_yua@nya.com>
2025-04-15 15:41:11 -0700

    added kidder suit

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
 rem start prjNya.exe --Home
 rem start prjNya.exe --PlayGirl
-rem start prjNya.exe --PlayTogether
-start prjNya.exe --Asteroid
+start prjNya.exe --PlayTogether
+rem start prjNya.exe --Asteroid
