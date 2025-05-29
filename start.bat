8065f3e53cb2cfc85db969b46020c28d8092147b
@serufu_yua <serufu_yua@nya.com>
2025-05-29 09:27:22 -0700

    added sounds for Play Together Scene

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
 rem start prjNya.exe --Home
-start prjNya.exe --PlayToy
+rem start prjNya.exe --PlayToy
 rem start prjNya.exe --PlaySolo
-rem start prjNya.exe --PlayTogether
+start prjNya.exe --PlayTogether
 rem start prjNya.exe --Asteroid
