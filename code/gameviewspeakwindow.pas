c179a82e62f702e5a5604254b71b12bd6a02242b
@serufu_yua <serufu_yua@nya.com>
2025-05-30 13:03:22 -0700

    self-close speak Window

--- code/gameviewspeakwindow.pas
+++ code/gameviewspeakwindow.pas
@@ -14,32 +14,29 @@ type
       private
         ImageBG: TCastleImageControl;
         ActorName, TextMessage: TCastleLabel;
-        FTime, FAppearTime, FLiveTime, FVanishTime: Single;
-        procedure SetTransparency(value: Single);
       public
         ParentView: TViewSpeakMenu;
         constructor Create(AOwner: TComponent;
                            chara: TNyaActorChara;
-                           message: String;
-                           timePerSymbol: Single); reintroduce;
-        //procedure Start; override;
-        procedure Update(const SecondsPassed: Single;
-                         var HandleInput: boolean); override;
+                           message: String); reintroduce;
+        procedure SetTransparency(value: Single);
       end;
     var
       FWin: TViewSpeakWindow;
       FChara: TNyaActorChara;
-      FTimePerSymbol: Single;
       FMessage: String;
+      FTime, FAppearTime, FLiveTime, FVanishTime: Single;
   public
     const
       DefaultAppearTime = 0.25;
-      DefaultLiveTime = 0.5;
+      DefaultLiveTime = 0.25;
       DefaultFVanishTime = 4.0;
 
     constructor CreateUntilStopped(chara: TNyaActorChara; message: String;
                                    timePerSymbol: Single = DefaultLiveTime);
     procedure Start; override;
+    procedure Update(const SecondsPassed: Single;
+                     var HandleInput: boolean); override;
   end;
 
 var
@@ -56,18 +53,13 @@ uses
 
 constructor TViewSpeakMenu.TViewSpeakWindow.Create(AOwner: TComponent;
                                                    chara: TNyaActorChara;
-                                                   message: String;
-                                                   timePerSymbol: Single);
+                                                   message: String);
 var
   UiOwner: TComponent;
   Ui: TCastleUserInterface;
   alpha: single;
 begin
   inherited Create(AOwner);
-  FTime:= 0.0;
-  FAppearTime:= DefaultAppearTime;
-  FLiveTime:= timePerSymbol * Length(message);
-  FVanishTime:= DefaultFVanishTime;
 
   // UiOwner is useful to keep reference to all components loaded from the design
   UiOwner := TComponent.Create(Self);
@@ -90,20 +82,6 @@ begin
   { set Color }
   alpha:= ImageBG.Color.W;
   ImageBG.Color:= Vector4(chara.PersonalColor, alpha);
-end;
-
-procedure TViewSpeakMenu.TViewSpeakWindow.Update(const SecondsPassed: Single;
-                                                 var HandleInput: boolean);
-begin
-  inherited;
-  { Executed every frame. }
-
-  FTime:= FTime + SecondsPassed;
-
-  if (FTime <= FAppearTime) then
-    SetTransparency(FTime / FAppearTime)
-  else if ((FTime >= (FAppearTime + FLiveTime)) AND (FTime <= (FAppearTime + FLiveTime + FVanishTime))) then
-    SetTransparency((FVanishTime - (FTime - (FAppearTime + FLiveTime))) / FVanishTime);
 end;
 
 procedure TViewSpeakMenu.TViewSpeakWindow.SetTransparency(value: Single);
@@ -134,18 +112,36 @@ begin
   inherited CreateUntilStopped;
   FChara:= chara;
   FMessage:= message;
-  FTimePerSymbol:= timePerSymbol;
+  FTime:= 0.0;
+  FAppearTime:= DefaultAppearTime;
+  FLiveTime:= timePerSymbol * Length(message);
+  FVanishTime:= DefaultFVanishTime;
 end;
 
 procedure TViewSpeakMenu.Start;
 begin
   inherited;
 
-  FWin:= TViewSpeakWindow.Create(FreeAtStop, FChara, FMessage, FTimePerSymbol);
-  FWin.ParentView := Self;
-  FWin.Anchor(hpMiddle);
-  FWin.Anchor(vpMiddle);
+  FWin:= TViewSpeakWindow.Create(FreeAtStop, FChara, FMessage);
+  FWin.ParentView:= Self;
+  FWin.Anchor(hpMiddle, 0);
+  FWin.Anchor(vpMiddle, 0);
   InsertFront(FWin);
+end;
+
+procedure TViewSpeakMenu.Update(const SecondsPassed: Single;
+                                var HandleInput: boolean);
+begin
+  inherited;
+
+  FTime:= FTime + SecondsPassed;
+
+  if (FTime <= FAppearTime) then
+    FWin.SetTransparency(FTime / FAppearTime)
+  else if ((FTime >= (FAppearTime + FLiveTime)) AND (FTime <= (FAppearTime + FLiveTime + FVanishTime))) then
+    FWin.SetTransparency((FVanishTime - (FTime - (FAppearTime + FLiveTime))) / FVanishTime)
+  else if (FTime > (FAppearTime + FLiveTime + FVanishTime)) then
+    Container.PopView(Self); { close }
 end;
 
 end.
