7da8e731497ae4432cb5476eaf6ad3e023a344be
@serufu_yua <serufu_yua@nya.com>
2025-05-30 14:22:50 -0700

    speaking in Solo Scene have repeats and timeouts

--- code/gameviewplaysolo.pas
+++ code/gameviewplaysolo.pas
@@ -7,12 +7,15 @@ uses
 
 type
   TViewPlaySolo = class(TBaseViewPlay)
+  protected
+    FTime: Single;
+    FSpeakOnceDone: Boolean;
+
+    procedure GetToGoBack; override;
+    procedure GiveSpeak;
   public
     procedure Start; override;
     procedure Update(const SecondsPassed: Single; var HandleInput: boolean); override;
-  protected
-    procedure GetToGoBack; override;
-    procedure GiveSpeak(const SecondsPassed: Single);
   end;
 
 var
@@ -27,6 +30,9 @@ uses
 
 procedure TViewPlaySolo.Start;
 begin
+  FTime:= 0.0;
+  FSpeakOnceDone:= True;
+
   { set map }
   Map.Url:= 'castle-data:/MapPlay_Solo.castle-user-interface';
 
@@ -38,29 +44,93 @@ end;
 
 procedure TViewPlaySolo.Update(const SecondsPassed: Single; var HandleInput: boolean);
 begin
-  GiveSpeak(SecondsPassed);
+  FTime:= FTime + SecondsPassed;
+  GiveSpeak;
 
   inherited;
 end;
 
-procedure TViewPlaySolo.GiveSpeak(const SecondsPassed: Single);
+procedure TViewPlaySolo.GiveSpeak;
+var
+  timeOut: Single;
+  i: integer;
 begin
   Case FActorsLogic.Status of
   TActorStatus.Start:
-    if FActorsLogic.StatusChanged then
-      Container.PushView(TViewSpeakMenu.CreateUntilStopped(FActorsLogic.Charas[0], 'Damn...'));
+    begin
+      if FActorsLogic.StatusChanged then
+      begin
+        FTime:= 0.0;
+        FSpeakOnceDone:= False;
+      end;
+
+      if NOT FSpeakOnceDone then
+      begin
+        timeOut:= RandomFloatRange(1.0, 3.0);
+        if (FTime > timeOut) then
+        begin
+          Container.PushView(TViewSpeakMenu.CreateUntilStopped(FActorsLogic.Charas[0], 'Damn...'));
+          FSpeakOnceDone:= True;
+          FTime:= 0.0;
+        end;
+      end;
+    end;
   TActorStatus.Go:
-    if FActorsLogic.StatusChanged then
-      Container.PushView(TViewSpeakMenu.CreateUntilStopped(FActorsLogic.Charas[0], 'A-a-ah.. Damn...'));
+    begin
+      if FActorsLogic.StatusChanged then
+      begin
+        Container.PushView(TViewSpeakMenu.CreateUntilStopped(FActorsLogic.Charas[0], 'A-ah... Damn...'));
+        FTime:= 0.0;
+      end;
+
+      timeOut:= RandomFloatRange(4.0, 16.0);
+      if (FTime > timeOut) then
+      begin
+        i:= RandomIntRangeInclusive(1, 3);
+        case i of
+          1: Container.PushView(TViewSpeakMenu.CreateUntilStopped(FActorsLogic.Charas[0], 'A-a-ah...'));
+          2: Container.PushView(TViewSpeakMenu.CreateUntilStopped(FActorsLogic.Charas[0], 'M-m-mph...'));
+          3: Container.PushView(TViewSpeakMenu.CreateUntilStopped(FActorsLogic.Charas[0], 'Yeah...'));
+        end;
+        FTime:= 0.0;
+      end;
+    end;
   TActorStatus.FastGo:
-    if FActorsLogic.StatusChanged then
-      Container.PushView(TViewSpeakMenu.CreateUntilStopped(FActorsLogic.Charas[0], 'Oh... M-m-m... C&apos;mon!'));
+    begin
+      if FActorsLogic.StatusChanged then
+      begin
+        Container.PushView(TViewSpeakMenu.CreateUntilStopped(FActorsLogic.Charas[0], 'O-o-oh!.. Come on!'));
+        FTime:= 0.0;
+      end;
+
+      timeOut:= RandomFloatRange(3, 8.0);
+      if (FTime > timeOut) then
+      begin
+        i:= RandomIntRangeInclusive(1, 3);
+        case i of
+          1: Container.PushView(TViewSpeakMenu.CreateUntilStopped(FActorsLogic.Charas[0], 'Argh!.. F-f-f!.. ', 0.125));
+          2: Container.PushView(TViewSpeakMenu.CreateUntilStopped(FActorsLogic.Charas[0], 'A-ah! Ayeah!', 0.125));
+          3: Container.PushView(TViewSpeakMenu.CreateUntilStopped(FActorsLogic.Charas[0], 'Come on!', 0.125));
+        end;
+        FTime:= 0.0;
+      end;
+    end;
   TActorStatus.Finish:
     if FActorsLogic.StatusChanged then
-      Container.PushView(TViewSpeakMenu.CreateUntilStopped(FActorsLogic.Charas[0], 'Nyaaaaa... ah...'));
+      Container.PushView(TViewSpeakMenu.CreateUntilStopped(FActorsLogic.Charas[0], 'Nyaaaaa!!. ah!..', 0.125));
   TActorStatus.Relax:
-    if FActorsLogic.StatusChanged then
-      Container.PushView(TViewSpeakMenu.CreateUntilStopped(FActorsLogic.Charas[0], 'Zzz...'));
+    begin
+      if FActorsLogic.StatusChanged then
+        FTime:= 0.0;
+
+      timeOut:= RandomFloatRange(6, 24.0);
+      if (FTime > timeOut) then
+      begin
+        Container.PushView(TViewSpeakMenu.CreateUntilStopped(FActorsLogic.Charas[0], 'Zzz...', 1.0));
+        FTime:= 0.0;
+      end;
+
+    end;
   end;
 end;
 
