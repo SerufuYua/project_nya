372f73a0cb8012d262b159b5fc3fb8af61f94e28
@serufu_yua <serufu_yua@nya.com>
2025-06-05 16:20:59 -0700

    simple design of Speech Bubble

--- code/nyaspeechbubble.pas
+++ code/nyaspeechbubble.pas
@@ -5,10 +5,13 @@ unit NyaSpeechBubble;
 interface
 
 uses
-  Classes, SysUtils, CastleUIControls, CastleClassUtils;
+  Classes, SysUtils, CastleUIControls, CastleClassUtils, CastleControls;
 
 type
   TNyaSpeechBubble = class(TCastleUserInterface)
+  protected
+    RectangleBG: TCastleRectangleControl;
+    ActorName, TextMessage: TCastleLabel;
   public
     constructor Create(AOwner: TComponent); override;
     procedure Update(const SecondsPassed: Single; var HandleInput: boolean); override;
@@ -18,11 +21,40 @@ type
 implementation
 
 uses
-  CastleComponentSerialize, CastleUtils;
+  CastleComponentSerialize, CastleUtils, CastleRectangles, CastleVectors;
 
 constructor TNyaSpeechBubble.Create(AOwner: TComponent);
+var
+  group: TCastlePackedGroup;
 begin
   inherited;
+  AutoSizeToChildren:= True;
+  HorizontalAnchorParent:= THorizontalPosition.hpMiddle;
+  HorizontalAnchorSelf:= THorizontalPosition.hpMiddle;
+  VerticalAnchorParent:= TVerticalPosition.vpMiddle;
+  VerticalAnchorSelf:= TVerticalPosition.vpMiddle;
+  Translation:= Vector2(0.0, 0.0);
+
+  RectangleBG:= TCastleRectangleControl.Create(self);
+  RectangleBG.SetTransient;
+  RectangleBG.AutoSizeToChildren:= True;
+  InsertFront(RectangleBG);
+
+  group:= TCastleVerticalGroup.Create(RectangleBG);
+  group.SetTransient;
+  group.Padding:= 16;
+  group.Spacing:= 14;
+  RectangleBG.InsertFront(group);
+
+  ActorName:= TCastleLabel.Create(group);
+  ActorName.SetTransient;
+  ActorName.Caption:= 'Actor:';
+  group.InsertFront(ActorName);
+
+  TextMessage:= TCastleLabel.Create(group);
+  TextMessage.SetTransient;
+  TextMessage.Caption:= 'Hello World!';
+  group.InsertFront(TextMessage);
 end;
 
 procedure TNyaSpeechBubble.Update(const SecondsPassed: Single; var HandleInput: boolean);
