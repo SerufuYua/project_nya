6998f86e7026f5b9fcde1ae6d0c23c7831b82144
@serufu_yua <serufu_yua@nya.com>
2025-07-31 13:36:38 -0700

    change girl personal color to Yellow

--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
 @echo off
-rem start prjNya.exe --Home
+start prjNya.exe --Home
 rem start prjNya.exe --PlayToy
 rem start prjNya.exe --PlaySolo
 rem start prjNya.exe --PlayTogether
 rem start prjNya.exe --Asteroid
-start prjNya.exe --Ride
+rem start prjNya.exe --Ride
