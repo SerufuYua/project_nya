f6b4434d294d3c22b0f04613cbc560161ac37f4c
@serufu_yua <serufu_yua@nya.com>
2025-08-07 13:33:31 -0700

    added Slime to The Road Asteroid

--- start.bat
+++ start.bat
@@ -3,6 +3,6 @@ rem start prjNya.exe --Home
 rem start prjNya.exe --PlayToy
 rem start prjNya.exe --PlaySolo
 rem start prjNya.exe --PlayTogether
-rem start prjNya.exe --Asteroid
+start prjNya.exe --Asteroid
 rem start prjNya.exe --Ride
-start prjNya.exe --Test
+rem start prjNya.exe --Test
