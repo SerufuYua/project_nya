a063795ffefd51607d5eb43b40ed4a9deffceff0
@serufu_yua <serufu_yua@nya.com>
2026-03-03 21:13:07 +0500

    multi-res icon fin

--- data/emblem/png2ico.bat
+++ data/emblem/png2ico.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Using ImageMagick (https://imagemagick.org) to convert PNGs to ICO
-magick emblem_256.png emblem_128.png emblem_064.png emblem_048.png emblem_032.png emblem_024.png -define icon emblem.ico
+magick emblem_512.png emblem_256.png emblem_128.png emblem_064.png emblem_048.png emblem_032.png emblem_024.png -define icon emblem.ico
 pause
